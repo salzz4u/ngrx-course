1a2d3fa7c43c77f06a6ab858e458af61029042df
salim <sgkapasi@gmail.com>
2020-03-29 14:02:44 -0400

    added basic auth and routing

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-import {Store} from "@ngrx/store";
-
-import {AuthService} from "../auth.service";
-import {tap} from "rxjs/operators";
-import {noop} from "rxjs";
-import {Router} from "@angular/router";
+import {AuthService} from '../auth.service';
+import {tap} from 'rxjs/operators';
+import {noop} from 'rxjs';
+import {Router} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../reducers';
 
 @Component({
   selector: 'login',
@@ -18,14 +18,15 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
 
   constructor(
-      private fb:FormBuilder,
-      private auth: AuthService,
-      private router:Router) {
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+    private  store: Store<AppState>) {
 
-      this.form = fb.group({
-          email: ['test@angular-university.io', [Validators.required]],
-          password: ['test', [Validators.required]]
-      });
+    this.form = fb.group({
+      email: ['test@angular-university.io', [Validators.required]],
+      password: ['test', [Validators.required]]
+    });
 
   }
 
@@ -34,6 +35,14 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    const val = this.form.value;
+    this.auth.login(val.email, val.password)
+      .pipe(tap(user => {
+        console.log(user);
+
+        this.router.navigateByUrl('/courses');
+      }))
+      .subscribe(noop, () => alert('login failed'));
 
   }
 
