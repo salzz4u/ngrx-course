47644734dd4fc9433dc38a23dd471af8cd2f92bc
salim <sgkapasi@gmail.com>
2020-03-29 17:09:02 -0400

    added auth route guard , added as provider in feature branch and root module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -9,6 +9,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {StoreModule} from '@ngrx/store';
 import {AuthService} from './auth.service';
 import * as fromAuth from './reducers';
+import {AuthGuard} from './auth.guard';
 
 @NgModule({
   imports: [
@@ -29,7 +30,8 @@ export class AuthModule {
     return {
       ngModule: AuthModule,
       providers: [
-        AuthService
+        AuthService,
+        AuthGuard
       ]
     };
   }
